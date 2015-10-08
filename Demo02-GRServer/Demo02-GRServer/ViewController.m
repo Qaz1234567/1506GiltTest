420b388801624eee4973ea3252a645a0e32e0253
tarena <tarena@seseguixianren.local>
2015-10-08 11:57:31 +0800

    54114353

---
+++ Demo02-GRServer/Demo02-GRServer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo02-GRServer
+//
+//  Created by tarena on 15/10/8.
+//  Copyright (c) 2015年 Mr.Lee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Demo02-GRServer/Demo02-GRServer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo02-GRServer
+//
+//  Created by tarena on 15/10/8.
+//  Copyright (c) 2015年 Mr.Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
